fb7065429b0d3b5f18da013f1ac700adad1e2e30
mwlodarc <m.wlodarczyk@stud.elka.pw.edu.pl>
2021-03-10 12:09:51 +0100

    Changed person repo

--- Laboratorium/README.md
+++ Laboratorium/README.md
@@ -4,3 +4,5 @@ Autorzy:
 * Marcin Dadura indeks: 303688 mail:01149350@pw.edu.pl
 * Maciej Włodarczyk indeks: 303725 mail:01149387@pw.edu.pl
 
+
+
--- Laboratorium/Repository/PersonRepository.cs
+++ Laboratorium/Repository/PersonRepository.cs
@@ -7,8 +7,8 @@ namespace Repository
     {
         private readonly Person[] shapes =
         {
-            new Patient("Ania", "Bodeńko", Sex.Female, "123123123"),
-            new Doctor("Maciej", "Kowalski", Sex.Male, "321321321"),
+            new Patient("Ania", "Koks", Sex.Female, "123123123"),
+            new Doctor("Pawel", "Kowalski", Sex.Male, "321321321"),
             new Patient("Ania", "Kozacka", Sex.Female, "321321321"),
             new Doctor("Krzysztof", "Kowal", Sex.Male, "1234748123"),
             new Patient("Marcin", "Rafał", Sex.Male, "32464371"),
