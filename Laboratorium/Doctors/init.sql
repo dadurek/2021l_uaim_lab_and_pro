0ae02a75a8fc6a97af471a1cd1cba292c8266f7b
Marcin Dadura <01149350@pw.edu.pl>
2021-03-27 23:53:09 +0100

    working init.sql in databases and downgrade version of Entity Framework

---
+++ Laboratorium/Doctors/init.sql
@@ -0,0 +1,80 @@
+BEGIN TRANSACTION;
+GO
+
+
+create table Doctors
+(
+    DoctorId  int identity
+        constraint PK_Doctors
+            primary key,
+    FirstName nvarchar(max),
+    LastName  nvarchar(max),
+    Sex       nvarchar(max) not null
+)
+go
+
+create table Specializations
+(
+    SpecId int identity
+        constraint PK_Specializations
+            primary key,
+    Number int not null
+)
+go
+
+create table DoctorSpecialization
+(
+    DoctorsDoctorId       int not null
+        constraint FK_DoctorSpecialization_Doctors_DoctorsDoctorId
+            references Doctors
+            on delete cascade,
+    SpecializationsSpecId int not null
+        constraint FK_DoctorSpecialization_Specializations_SpecializationsSpecId
+            references Specializations
+            on delete cascade,
+    constraint PK_DoctorSpecialization
+        primary key (DoctorsDoctorId, SpecializationsSpecId)
+)
+go
+
+create index IX_DoctorSpecialization_SpecializationsSpecId
+    on DoctorSpecialization (SpecializationsSpecId)
+go
+
+insert into Specializations (Number) VALUES (1)
+insert into Specializations (Number) VALUES (2)
+insert into Specializations (Number) VALUES (3)
+insert into Specializations (Number) VALUES (4)
+insert into Specializations (Number) VALUES (5)
+insert into Specializations (Number) VALUES (6)
+insert into Specializations (Number) VALUES (7)
+insert into Specializations (Number) VALUES (8)
+insert into Specializations (Number) VALUES (9)
+insert into Specializations (Number) VALUES (10)
+insert into Specializations (Number) VALUES (11)
+insert into Specializations (Number) VALUES (12)
+insert into Specializations (Number) VALUES (13)
+insert into Specializations (Number) VALUES (14)
+insert into Specializations (Number) VALUES (15)
+insert into Specializations (Number) VALUES (16)
+insert into Specializations (Number) VALUES (17)
+insert into Specializations (Number) VALUES (18)
+insert into Specializations (Number) VALUES (19)
+insert into Specializations (Number) VALUES (20)
+
+insert into Doctors (FirstName, LastName, Sex) values ('Marcin','Dadura','Male')
+insert into Doctors (FirstName, LastName, Sex) values ('Maciej','Wlodarczyk','Female')
+
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (1,1)
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (1,2)
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (1,3)
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (1,8)
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (1,9)
+
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (2,4)
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (2,5)
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (2,11)
+insert into DoctorSpecialization (DoctorsDoctorId, SpecializationsSpecId) values (2,12)
+
+COMMIT;
+GO
---
+++ Laboratorium/Doctors/setup.sh
@@ -0,0 +1,4 @@
+sleep 20 #if doesnt work increase time xdddd
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P zaq1@WSX -i /init.sql
+
