5f83402ecaa8fc9d768dc61e3b30bcb5b8b5a3ea
Marcin Dadura <m.dadura@stud.elka.pw.edu.pl>
2021-05-06 11:43:52 +0200

    Update docker-compose-lab.yml and compose.bat

--- Miniprojekty/Docker/compose.bat
+++ Miniprojekty/Docker/compose.bat
@@ -1,7 +1,7 @@
 docker-compose -f docker-compose-lab.yml up -d
 timeout /t 20
 
-curl -X GET "https://localhost:5001/examination-rooms-selection" -H  "accept: text/plain"
-curl -X POST "https://localhost:5001/add-doctor" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"FirstName\":\"Michal\",\"LastName\":\"Jakis\",\"Sex\":\"male\",\"Specializations\":[1,5]}"
-curl -X POST "https://localhost:5001/add-room" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"Number\":\"99a\",\"Certifications\":[1,5]}"
-curl -X GET "https://localhost:5001/examination-rooms-selection" -H  "accept: text/plain"
+curl -X GET "https://localhost:44328/examination-rooms-selection" -H  "accept: text/plain"
+curl -X POST "https://localhost:44328/add-doctor" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"FirstName\":\"Michal\",\"LastName\":\"Jakis\",\"Sex\":\"male\",\"Specializations\":[1,5]}"
+curl -X POST "https://localhost:44328/add-room" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"Number\":\"99a\",\"Certifications\":[1,5]}"
+curl -X GET "https://localhost:44328/examination-rooms-selection" -H  "accept: text/plain"
