3b378c8eb8186de846a95adeb83135727d3a8119
Marcin Dadura <m.dadura@stud.elka.pw.edu.pl>
2021-05-23 14:58:45 +0200

    Fix NetworkClientFactory in WebApp-lab

--- Laboratorium/WebApp/Model/Service/NetworkClientFactory.cs
+++ Laboratorium/WebApp/Model/Service/NetworkClientFactory.cs
@@ -5,20 +5,12 @@
         public static INetwork GetNetworkClient()
         {
 #if DEBUG
-            /* AT
-            const string serviceHost = "localhost";
-            const int servicePort = 8081;
-      
-            return new NetworkClient( serviceHost, servicePort );
-            */
             return new FakeNetworkClient();
-
 #else
             const string serviceHost = "roomselector-app";
             const int servicePort = 80;
 
             return new NetworkClient( serviceHost, servicePort );
-
 #endif
         }
     }
